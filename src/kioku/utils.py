6968e94747d5a23aabec722a782b4ce5cf36d266
Daniele Tricoli <eriol@mornie.org>
2020-11-08 04:14:04 +0100

    Add a simple loader for images

---
+++ src/kioku/utils.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from random import shuffle
+
+
+class ImageLoader:
+    def __init__(self, path):
+        self.path = Path(path)
+        self.images = []
+
+        self.load()
+
+    def load(self):
+        self.images = [f.name for f in self.path.glob("*.jpg")]
+        print(self.images)
+
+    def get_repeated_images(self, times):
+        images_to_return = self.images * 2 * times
+        shuffle(images_to_return)
+
+        for image in images_to_return:
+            yield str(self.path / image)
