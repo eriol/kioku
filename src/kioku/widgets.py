f0e917a8732eb2b6999c78e86403a89f1b2ea04e
Daniele Tricoli <eriol@mornie.org>
2020-11-09 00:28:53 +0100

    Add animation to card

--- src/kioku/widgets.py
+++ src/kioku/widgets.py
@@ -1,6 +1,7 @@
-from kivy.uix.button import Button
-
+from kivy.animation import Animation
+from kivy.clock import Clock
 from kivy.properties import NumericProperty, StringProperty, BooleanProperty
+from kivy.uix.button import Button
 
 
 class Card(Button):
@@ -17,5 +18,19 @@ class Card(Button):
             self.background_normal = self.card_image
 
     def reset(self):
+        self._fade_out()
+
+    def _fade_out(self):
+        anim = Animation(opacity=0)
+        anim.bind(on_complete=self._reset)
+        anim.start(self)
+
+    def _reset(self, *args):
         self.text = "記憶"
         self.background_normal = "atlas://data/images/defaulttheme/button"
+
+        Clock.schedule_once(self._fade_in)
+
+    def _fade_in(self, *args):
+        anim = Animation(opacity=1)
+        anim.start(self)
