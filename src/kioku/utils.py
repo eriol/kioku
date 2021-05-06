9232f383600e7d43e9b7caf084eea4f6f42217d7
Daniele Tricoli <eriol@mornie.org>
2021-05-07 00:09:30 +0200

    Add docstring and move glob pattern into an external variable

--- src/kioku/utils.py
+++ src/kioku/utils.py
@@ -1,8 +1,13 @@
 from pathlib import Path
 from random import shuffle
 
+# It's OK to keep this simple and support only JPG.
+ALLOWED_IMAGES_PATTERN = "*.jpg"
+
 
 class ImageLoader:
+    """Load images from the specified path."""
+
     def __init__(self, path):
         self.path = Path(path)
         self.images = []
@@ -10,7 +15,7 @@ class ImageLoader:
         self.load()
 
     def load(self):
-        self.images = [f.name for f in self.path.glob("*.jpg")]
+        self.images = [f.name for f in self.path.glob(ALLOWED_IMAGES_PATTERN)]
 
     def get_repeated_images(self, times):
         images_to_return = self.images * 2 * times
