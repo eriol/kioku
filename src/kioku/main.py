db2eeb5a731d9bedf378e86a0c115465e7b2bc91
Daniele Tricoli <eriol@mornie.org>
2021-05-08 00:01:40 +0200

    Add a main toolbar to the game

--- src/kioku/main.py
+++ src/kioku/main.py
@@ -1,15 +1,21 @@
 """Kioku Memory Game."""
 from kivy.core.window import Window
 from kivymd.app import MDApp
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.screen import MDScreen
 
-from kioku.screens import GameScreen, SettingsScreen  # noqa: F401
+from .screens import GameScreen, SettingsScreen  # noqa: F401
 
 Window.size = (700, 900)
 
 
 class MainScreen(MDScreen):
     """Main screen of the game."""
+
+
+class MainToolbar(ThemableBehavior, MDBoxLayout):
+    """The main toolbar of the game."""
 
 
 class KiokuApp(MDApp):
