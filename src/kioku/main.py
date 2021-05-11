c49c68cc663a8566a6384c000ce7f89bd836841f
Daniele Tricoli <eriol@mornie.org>
2021-05-11 18:38:22 +0200

    Add a button to delete levels
    Use a dialog to confirm the action.

--- src/kioku/main.py
+++ src/kioku/main.py
@@ -3,6 +3,8 @@ from kivy.core.window import Window
 from kivymd.app import MDApp
 from kivymd.theming import ThemableBehavior
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDFlatButton
+from kivymd.uix.dialog import MDDialog
 from kivymd.uix.screen import MDScreen
 
 from .config import settings
@@ -23,6 +25,8 @@ class MainToolbar(ThemableBehavior, MDBoxLayout):
 class KiokuApp(MDApp):
     """Kioku application."""
 
+    dialog = None
+
     def on_start(self):
         self.load_levels()
 
@@ -33,6 +37,30 @@ class KiokuApp(MDApp):
                 self.root.get_screen("main").ids.levels.add_widget(
                     LevelCoverListItem.from_metadata(level_path)
                 )
+
+    def show_alert_delete_level_dialog(self, level_name):
+        if not self.dialog:
+            self.dialog = MDDialog(
+                text=f"Delete {level_name} level?",
+                buttons=[
+                    MDFlatButton(
+                        text="CANCEL",
+                        text_color=self.theme_cls.primary_color,
+                        on_press=self.close_alert_delete_level_dialog,
+                    ),
+                    MDFlatButton(
+                        text="DELETE", text_color=self.theme_cls.primary_color
+                    ),
+                ],
+            )
+        self.dialog.bind(on_dismiss=self.on_alert_delete_level_dialog_dismiss)
+        self.dialog.open()
+
+    def close_alert_delete_level_dialog(self, *args):
+        self.dialog.dismiss()
+
+    def on_alert_delete_level_dialog_dismiss(self, *args):
+        self.dialog = None
 
 
 def main():
