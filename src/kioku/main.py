8a65f740da84711dee1ebc07637276799d41d3f0
Daniele Tricoli <eriol@mornie.org>
2021-05-13 02:51:57 +0200

    Delete a level

--- src/kioku/main.py
+++ src/kioku/main.py
@@ -1,4 +1,6 @@
 """Kioku Memory Game."""
+import shutil
+
 from kivy.core.window import Window
 from kivymd.app import MDApp
 from kivymd.theming import ThemableBehavior
@@ -26,6 +28,7 @@ class KiokuApp(MDApp):
     """Kioku application."""
 
     dialog = None
+    selected_level_path = None
 
     def on_start(self):
         self.load_levels()
@@ -38,10 +41,11 @@ class KiokuApp(MDApp):
                     LevelCoverListItem.from_metadata(level_path)
                 )
 
-    def show_alert_delete_level_dialog(self, level_name):
+    def show_alert_delete_level_dialog(self, level_name, level_path):
         if not self.dialog:
             self.dialog = MDDialog(
-                text=f"Delete {level_name} level?",
+                title=f"Delete {level_name} level",
+                text=f"Are you sure you want to delete {level_name} level?",
                 buttons=[
                     MDFlatButton(
                         text="CANCEL",
@@ -49,11 +53,16 @@ class KiokuApp(MDApp):
                         on_press=self.close_alert_delete_level_dialog,
                     ),
                     MDFlatButton(
-                        text="DELETE", text_color=self.theme_cls.primary_color
+                        text="DELETE",
+                        text_color=self.theme_cls.primary_color,
+                        on_press=self.close_alert_delete_level,
                     ),
                 ],
             )
+
+        self.selected_level_path = level_path
         self.dialog.bind(on_dismiss=self.on_alert_delete_level_dialog_dismiss)
+        self.dialog.set_normal_height()
         self.dialog.open()
 
     def close_alert_delete_level_dialog(self, *args):
@@ -61,6 +70,13 @@ class KiokuApp(MDApp):
 
     def on_alert_delete_level_dialog_dismiss(self, *args):
         self.dialog = None
+        self.selected_level_path = None
+
+    def close_alert_delete_level(self, *args):
+        shutil.rmtree(self.selected_level_path)
+        self.root.get_screen("main").ids.levels.clear_widgets()
+        self.load_levels()
+        self.dialog.dismiss()
 
 
 def main():
